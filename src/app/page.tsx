8dcb90b2edb309acfb892eef113924f3333e8796
PramodAbhiramMutyala <pramodabhiramm@gmail.com>
2026-04-05 18:42:30 +0530

    feat: switch homepage to chat style prompt interface

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { ChatBox } from "@/components/chat/ChatBox";
+
+/**
+ * Render a chat-first interface for generating and downloading presentations.
+ */
+export default function Home() {
+  return <ChatBox />;
+}
